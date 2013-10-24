02feaf883719fe9ba37e088d9bdc3a1f91acbba8
Bogdan Marchenko <me@bmarchenko.com>
2013-10-24 20:15:45 +0300

    fixes

--- polls/models.py
+++ polls/models.py
@@ -71,4 +71,4 @@ class Poll(models.Model):
     user = models.ForeignKey(Profile)
 
     def __unicode__(self):
-        return "{0} - {1}".format(self.place.title, self.date)
+        return "{0} - {1}".format(self.place.title, self.user.name)
